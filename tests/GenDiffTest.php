29d698251313342f81d1ceaca5c68a80b3a44cee
Alexey-654 <vianosenko@gmail.com>
2020-06-11 20:59:09 +0300

    deleted one test assertion

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -11,11 +11,6 @@ class GenDiffTest extends TestCase
 {
     public function testGenDiff()
     {
-        $pathToFile1 = '/home/alexey/php-project-lvl2/tests/fixtures/before.json';
-        $pathToFile2 = '/home/alexey/php-project-lvl2/tests/fixtures/after.json';
-        $result1 = file_get_contents('/home/alexey/php-project-lvl2/tests/fixtures/result.json');
-        $this->assertEquals($result1, genDiff($pathToFile1, $pathToFile2));
-
         $pathToFile2 = 'tests/fixtures/before.json';
         $pathToFile3 = 'tests/fixtures/after.json';
         $result2 = file_get_contents('tests/fixtures/result.json');
